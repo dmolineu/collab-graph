3d78b016b7cd9c5f8f605aab30e18416cb077347
D.L. Molineu <dmolineu@gmail.com>
2018-02-25 13:14:03 -0600

    Initial commit

---
+++ src/main/java/com/dlmol/collabgraph/CollabGraphApplication.java
@@ -0,0 +1,12 @@
+package com.dlmol.collabgraph;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabGraphApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabGraphApplication.class, args);
+	}
+}
